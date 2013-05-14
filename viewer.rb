a6c00a3f3d7cf7b1a16b4506f62732be0d653b96
Ari Brown <ari@aribrown.com>
2013-05-14 10:23:18 -0400

    settling on a format for the logviewer

--- viewer.rb
+++ viewer.rb
@@ -1,52 +1,67 @@
 require 'sinatra'
-require 'sinatra/async'
+
+class Log
+  attr_accessor :log
+
+  def initialize
+    @log = ""
+  end
+
+  def clear
+    @log.clear
+  end
+
+  def prepend(*pieces)
+    [*pieces].reverse.each do |piece|
+      log.prepend piece
+      log.prepend "<br />"
+    end
+  end
+
+  def connect(cdn, command)
+    prepend "Connecting to #{cdn}, executing #{command}"
+  end
+
+  def poll(cdn)
+    prepend "Polling #{cdn}"
+  end
+
+  def other(line)
+    prepend line
+  end
+end
 
 # Sinatra does funky magic with class-methods and instance-methods
 # and I don't really want to spend the time to figure it out and make
 # it better right now. Settling for a copout.
-LOG = ''
+# # Sinatra does funky magic with class-methods and instance-methods
+# and I don't really want to spend the time to figure it out and make
+# it better right now. Settling for a copout.
+LOG = Log.new
+
+Thread.new do
+  STDIN.each_line do |line|
+    case line
+    when /^connecting to (.+)/i
+      parts = $1.split('|').map {|p| p.strip }
+      LOG.connect parts[0], parts[1..-1].join('|')
+    when /polling cdn (.+)/i
+      LOG.poll $1
+    else
+      LOG.other line.inspect
+    end
+  end
+end
 
 class LogViewer < Sinatra::Base
-  register Sinatra::Async
-
-  get '/' do
-    log
+  get '/log' do
+    LOG.log
   end
 
   get '/clear' do
     LOG.clear
   end
-
-  def connect(cdn, command)
-    LOG << "Connecting to #{cdn}, executing #{command}"
-    LOG << "<br />"
-  end
-
-  def poll(cdn)
-    LOG << "Polling #{cdn}"
-    LOG << "<br />"
-  end
-
-  def other(line)
-    LOG << line
-    LOG << "<br />"
-  end
 end
 
-def log
-  LogViewer.new!
-end
-
-Thread.new do
-  STDIN.each_line do |line|
-    case line
-    when /connecting to (.+) | (.*)/i
-      log.connect $1, $2
-    when /polling cdn (.+)/i
-      log.poll $1
-    else
-      log.other line
-    end
-  end
-end
+LogViewer.run!
 
