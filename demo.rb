8b5269f0d4ace4bbae066a6f7ee90ad56c95ff7c
Ari Brown <ari@aribrown.com>
2013-05-13 21:44:38 -0400

    ok, got everything appropriately split up. i'm happy with the threaded_each and blocks i've added

--- demo.rb
+++ demo.rb
@@ -5,35 +5,46 @@ require 'redis'
 require 'thread'
 require 'net/ssh'
 
-REDIS = Redis.new "/tmp/demo.sock"
-
-def poll(cdn)
-  # The system shall save the JSON configuration file in a meaningful way in a
-  # Reddis.io data store upon successful poll.
-  data = JSON.load open(cdn)
-  REDIS.set cdn, data.to_json
-
-  follow_config data
+module Enumerable
+  def threaded_each(&blk)
+    map {|i| Thread.new { blk.call i } }
+  end
 end
 
-def follow_config(data)
-  # The system shall invoke a post script on every server specified in the JSON
-  # configuration upon successful poll (simulate this, but show code to SSH into
-  # a server).
-  data['servers'].each do |server|
-    Thread.new do
-      Net::SSH.start server,
-                     data['server_admins'].first,
-                     :password => "password" do |ssh|
-        ssh.exec data['post_script']
+def poll(cdn, &blk)
+  puts "Polling CDN"
+  data = JSON.load open(cdn)
+  blk.call data
+  data
+end
+
+def connect_to(server, login, password, &blk)
+  puts "Connecting to #{login}:***@#{server}"
+  #Net::SSH.start(server, login, :password => password) {|ssh| blk.call ssh }
+end
+
+# The system shall poll 4 CDNs for a JSON configuration file.
+cdns  = STDIN.read.split
+
+REDIS = Redis.new "/tmp/demo.sock"
+
+# The system shall poll every CDN in 5 minute intervals.
+timers.every(5 * 60) do
+  cdns.threaded_each do |cdn|
+    poll cdn do |data|
+      # The system shall save the JSON configuration file in a meaningful way
+      # in a Reddis.io data store upon successful poll.
+      REDIS.set cdn, data.to_json
+
+      # The system shall invoke a post script on every server specified in
+      # the JSON configuration upon successful poll (simulate this, but show
+      # code to SSH into a server).
+      data['servers'].threaded_each do |server|
+        connect_to server, data['server_admins'].first, "password" do |ssh|
+          ssh.exec data['post_script']
+        end
       end
     end
   end
 end
-
-# The system shall poll 4 CDNs for a JSON configuration file.
-cdns = STDIN.read.split
-
-# The system shall poll every CDN in 5 minute intervals.
-timers.every(5 * 60) { cdns.each {|cdn| Thread.new { poll cdn } } }
 
